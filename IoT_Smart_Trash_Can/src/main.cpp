f18fb75197f2d9b2a02c8c290cd26680b9fe3be3
et20068PS <118202288+et20068PS@users.noreply.github.com>
2023-03-24 19:36:15 +0100

    implemented deep sleep mode

--- IoT_Smart_Trash_Can/src/main.cpp
+++ IoT_Smart_Trash_Can/src/main.cpp
@@ -1,8 +1,8 @@
 #include <Arduino.h>
 #include <WiFi.h>
-
 #include <AdafruitIO_WiFi.h>
 #include <AdafruitIO_Feed.h>
+
 #define AIO_USERNAME  "SmartTrashCan"
 #define AIO_KEY       "aio_UJVn26FOOOAQZAqOZlDbZWO54NXE"
 #define AIO_SERVER      "io.adafruit.com"
@@ -12,17 +12,22 @@
 const char* ssid = "LenovoPS";
 const char* password =  "#Lu89832";
 
-//MQTT client variables
-WiFiClient wifiClient;
-
+//Timer variables
 long last_time;
 long now;
 
+//Adafruit IO connection variables
 AdafruitIO_WiFi io(AIO_USERNAME, AIO_KEY, ssid, password);
+
+//Adafruit Topics for sensors
 AdafruitIO_Feed *dataFillingLevel = io.feed("fill");
 AdafruitIO_Feed *dataHumidity = io.feed("humidity");
 AdafruitIO_Feed *dataTemperature = io.feed("temperature");
 AdafruitIO_Feed *dataVOC = io.feed("air");
+
+//Adafruit Topics for remote control
+AdafruitIO_Feed *dataBucketHeight = io.feed("bucketheight");
+AdafruitIO_Feed *dataSaveEnergy = io.feed("saveenergy");
 
 //Sensor measurement variables
 float sensorTemperature;
@@ -30,24 +35,67 @@ float sensorHumidity;
 float sensorVOC;
 int sensorFillingLevel;
 
-//Limits
+//Limit variables
 float limitHumidity = 50;
 float limitVOC = 3000;
+int bucketHeight = 50;
 boolean criticalWarning = false;
+
+boolean saveEnergy;
 
 //WiFi connect function
 void connectWiFi(){
+  Serial.print("Connecting to WiFi");
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
-    Serial.println("Connecting to WiFi..");
+    Serial.print(".");
   }
+  Serial.println();
   Serial.println("Connected to the WiFi network");
 }
 
+//WiFi disconnect function
+void disconnectWiFi(){
+  Serial.print("Disonnecting WiFi");
+  while (WiFi.status() == WL_CONNECTED) {
+    WiFi.disconnect();
+    WiFi.mode(WIFI_OFF);
+    delay(1000);
+    Serial.print(".");
+  }
+  Serial.println();
+  Serial.println("Disconnected from WiFi network");
+}
 
+//Read actual sensor data
+void readSensors(){
+    //generating random sensor data for testing
+    sensorTemperature = 1.353456836 * random(30.0); //max. Wert random Funktion 30 factor 1.353456836 for float generation, random only creates integer
+    sensorHumidity = 1.353456836 * random(70.0); //max. Wert random Funktion 70
+    sensorVOC = 1.68364967 * random(2000.0);
+    sensorFillingLevel = random(100.0); 
+}
+
+//Publish data to Adafruit IO
+void publishData(){
+    dataFillingLevel->save(sensorFillingLevel);
+    dataHumidity->save(sensorHumidity);
+    dataTemperature->save(sensorTemperature);
+    dataVOC->save(sensorVOC);
+}
+
+//Function to process received data from Adafruit IO Dashboard
 void handleFeedData(AdafruitIO_Data *data) {
-  Serial.println("Received feed data:");
+  Serial.print("Received data from feed: ");
+  Serial.println(data->feedName());
   Serial.println(data->value());
+
+  if(String(data->feedName()) == "saveenergy"){
+    saveEnergy = data->toBool();
+  }
+  else if(String(data->feedName()) == "bucketheight"){
+    bucketHeight = data->toInt();
+  }
 }
 
 void setup() {
@@ -65,36 +113,51 @@ void setup() {
     Serial.print(".");
     delay(500);
   }
-  //conncetion status
+  //Adafruit IO conncetion status
+  Serial.println();
   Serial.println(io.statusText());
 
-  //toggleSwitch->onMessage(handleFeedData);
-  //toggleSwitch->get();
+  io.run();
+
+  //Receive data from Adafruit IO Dashboard
+  dataBucketHeight->onMessage(handleFeedData);
+  dataBucketHeight->get();
+  dataSaveEnergy->onMessage(handleFeedData);
+  dataSaveEnergy->get();
 }
 
 void loop() {
 
-  if (!WiFi.isConnected()){
-    connectWiFi();
-  }
-
+  //Must be called frequently to keep Adafruit IO connection alive
   io.run();
 
   now = millis();
-  if (now - last_time > 5000)
+  if (!saveEnergy && now - last_time > 10000)
   {
-      sensorTemperature = 1.353456836 * random(30.0); //max. Wert random Funktion 30 factor 1.353456836 for float generation, random only creates integer
-      sensorHumidity = 1.353456836 * random(70.0); //max. Wert random Funktion 70
-      sensorVOC = 1.68364967 * random(2000.0);
-      sensorFillingLevel = random(100.0); 
-     
+      if (!WiFi.isConnected()){
+        connectWiFi();
+      }
 
-      dataFillingLevel->save(sensorFillingLevel);
-      dataHumidity->save(sensorHumidity);
-      dataTemperature->save(sensorTemperature);
-      dataVOC->save(sensorVOC);
+      readSensors();
+      //publishing sensor data to Adafruit IO
+      publishData();
 
       last_time = now;
+  }
+  else if(saveEnergy){
+
+    readSensors();
+    //publishing sensor data to Adafruit IO
+    publishData();
+
+    //Sleep mode implementation
+    //Stays in sleep mode until timer wakes up the ESP32
+    //When woke up, ESP32 starts from the beginning of the code and reinitializes
+    Serial.println("Ich geh schlafen");
+    disconnectWiFi();
+    esp_sleep_enable_timer_wakeup(1 * 60 * 1000000); //1 * 60 * 1000000 = 1 minute
+    esp_deep_sleep_start();
+
   }
   
 }
